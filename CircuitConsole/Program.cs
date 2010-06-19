95a9dc7995272aaecf24604e88b7fb7b4bcab50d
tstivers <tstivers@f32c2155-1bf7-4310-8653-851265ff3829>
2010-06-19 05:07:53 +0000

    * checking in the solution

---
+++ CircuitConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Circuits;
+
+namespace CircuitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circuit circuit = new Circuit();
+            Gate gate0 = circuit.AddGate(0);
+            Gate gate1 = circuit.AddGate(1);
+            gate0.InputL = circuit.InputWire;
+            gate0.InputR.ConnectTo()
+        }
+    }
+}
